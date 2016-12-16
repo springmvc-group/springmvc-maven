02d7bb044826a0ae0bd2eb32bd69e80fcd696938
fengtaiguang <osiit@163.com>
2016-12-16 08:13:48 +0800

    上传

--- src/main/java/com/fengtaiguang/springmvc/portal/web/controller/spring/UserControllerSpring.java
+++ src/main/java/com/fengtaiguang/springmvc/portal/web/controller/spring/UserControllerSpring.java
@@ -1,9 +1,16 @@
 package com.fengtaiguang.springmvc.portal.web.controller.spring;
 
+import java.io.File;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.my.sys.entity.User;
 
@@ -51,5 +58,26 @@ public class UserControllerSpring {
 		user.setUserId("u001");
 		return user;
 	}
-
+	@RequestMapping(value = "update")  
+    public String upload(@RequestParam(value = "file", required = false) MultipartFile file, HttpServletRequest request, ModelMap model) {  
+  
+        System.out.println("开始");  
+        String path = request.getSession().getServletContext().getRealPath("upload");  
+        String fileName = file.getOriginalFilename();  
+//        String fileName = new Date().getTime()+".jpg";  
+        System.out.println(path);  
+        File targetFile = new File(path, fileName);  
+        if(!targetFile.exists()){  
+            targetFile.mkdirs();  
+        }  
+        //保存  
+        try {  
+            file.transferTo(targetFile);  
+        } catch (Exception e) {  
+            e.printStackTrace();  
+        }  
+        model.addAttribute("fileUrl", request.getContextPath()+"/upload/"+fileName);  
+  
+        return "result";  
+    }  
 }
